9353df3564cc5d014c3d056428c29c16f01d762b
Alfonso Garcia <alfonso.garcia@remberg.de>
2025-01-19 21:13:23 +0100

    fix build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimationsAsync()],
+  providers: [provideAnimationsAsync(), provideRouter([], withHashLocation())],
 };
